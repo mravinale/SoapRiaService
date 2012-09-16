368da6af9fbcfcbedd14093caf09b4f5a3b285ce
Mariano Ravinale <mravinale@gmail.com>
2012-09-16 11:44:35 -0300

    initial commit

---
+++ SoapRiaService/SoapRiaService/Services/EmployeeDomainService.cs
@@ -0,0 +1,58 @@
+﻿
+namespace SoapRiaService {
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.ComponentModel.DataAnnotations;
+    using System.Data;
+    using System.Linq;
+    using System.ServiceModel.DomainServices.EntityFramework;
+    using System.ServiceModel.DomainServices.Hosting;
+    using System.ServiceModel.DomainServices.Server;
+    using SoapRiaService.Models;
+
+
+    // Implements application logic using the SoapTestDbEntities context.
+    // TODO: Add your application logic to these methods or in additional methods.
+    // TODO: Wire up authentication (Windows/ASP.NET Forms) and uncomment the following to disable anonymous access
+    // Also consider adding roles to restrict access as appropriate.
+    // [RequiresAuthentication]
+    [EnableClientAccess()]
+    public class EmployeeDomainService : LinqToEntitiesDomainService<SoapTestDbEntities> {
+
+        // TODO:
+        // Consider constraining the results of your query method.  If you need additional input you can
+        // add parameters to this method or create additional query methods with different names.
+        // To support paging you will need to add ordering to the 'Employees' query.
+        [Invoke]
+        [Query(IsDefault = true)]
+        public IQueryable<Employee> GetEmployees() {
+            return this.ObjectContext.Employees;
+        }
+
+        [Invoke]
+        public void InsertEmployee(Employee employee) {
+            if ((employee.EntityState != EntityState.Detached)) {
+                this.ObjectContext.ObjectStateManager.ChangeObjectState(employee, EntityState.Added);
+            }
+            else {
+                this.ObjectContext.Employees.AddObject(employee);
+            }
+        }
+
+        [Invoke]
+        public void UpdateEmployee(Employee currentEmployee) {
+            this.ObjectContext.Employees.AttachAsModified(currentEmployee, this.ChangeSet.GetOriginal(currentEmployee));
+        }
+
+        [Invoke]
+        public void DeleteEmployee(Employee employee) {
+            if ((employee.EntityState == EntityState.Detached)) {
+                this.ObjectContext.Employees.Attach(employee);
+            }
+            this.ObjectContext.Employees.DeleteObject(employee);
+        }
+    }
+}
+
+
---
+++ SoapRiaService/SoapRiaService/Services/EmployeeDomainService.metadata.cs
@@ -0,0 +1,43 @@
+﻿
+namespace SoapRiaService.Models {
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.ComponentModel.DataAnnotations;
+    using System.Linq;
+    using System.ServiceModel.DomainServices.Hosting;
+    using System.ServiceModel.DomainServices.Server;
+
+
+    // The MetadataTypeAttribute identifies EmployeeMetadata as the class
+    // that carries additional metadata for the Employee class.
+    [MetadataTypeAttribute(typeof(Employee.EmployeeMetadata))]
+    public partial class Employee {
+
+        // This class allows you to attach custom attributes to properties
+        // of the Employee class.
+        //
+        // For example, the following marks the Xyz property as a
+        // required property and specifies the format for valid values:
+        //    [Required]
+        //    [RegularExpression("[A-Z][A-Za-z0-9]*")]
+        //    [StringLength(32)]
+        //    public string Xyz { get; set; }
+        internal sealed class EmployeeMetadata {
+
+            // Metadata classes are not meant to be instantiated.
+            private EmployeeMetadata() {
+            }
+
+            public string Comments { get; set; }
+
+            public int Id { get; set; }
+
+            public string LastName { get; set; }
+
+            public string Name { get; set; }
+
+            public Nullable<int> SID { get; set; }
+        }
+    }
+}
